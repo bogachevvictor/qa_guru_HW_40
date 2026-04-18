62e52f2addebcabdf413b1c0a74220789768ffa8
Victor Bogachev <mapku389@gmail.com>
2026-04-18 16:21:55 +0300

    Добавлены отчётность Allure Reports, подключение к Jenkins, вложения в коде (скриншоты, логи, видео)

--- src/test/java/guru/qa/TestBase.java
+++ src/test/java/guru/qa/TestBase.java
@@ -27,7 +27,7 @@ public class TestBase {
 //        Configuration.pageLoadStrategy = "eager";
 //        Configuration.holdBrowserOpen = true;
 //        Configuration.timeout = 5000; // default 4000
-        Configuration.remote = "https://user1:1234@selenoid.autotest.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
